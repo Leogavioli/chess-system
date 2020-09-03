c8fb1f2a917568aa6dd83f0cd57bdf0017b9b1d1
Leogavioli <gaviolileo@gmail.com>
2020-09-03 10:32:39 -0300

    Created classes: Piece, Board, ChessPiece, ChessMatch, UI, and enum Color

--- src/appliction/Program.java
+++ src/appliction/Program.java
@@ -1,10 +1,15 @@
 package appliction;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá");
 
+
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
